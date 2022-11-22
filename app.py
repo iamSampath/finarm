6ffc98cdd72ce6534b5ab20858d2024ac85b50c9
Sampath Medarametla <reachtosampath@gmail.com>
2022-11-21 23:03:21 -0600

    Loginform implemented

--- app.py
+++ app.py
@@ -14,3 +14,8 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=['GET'])
+def login():
+    return render_template("login.html")
