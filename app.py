9707f98716d12c124014dc532075d2dcaab46bdd
Sampath Medarametla <reachtosampath@gmail.com>
2022-11-29 21:15:36 -0600

    Registration page

--- app.py
+++ app.py
@@ -19,3 +19,7 @@ def index():
 @app.route("/login", methods=['GET'])
 def login():
     return render_template("login.html")
+
+@app.route("/register", methods=['GET'])
+def register():
+    return render_template("registration.html")
